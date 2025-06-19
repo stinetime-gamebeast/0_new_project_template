bfe02e1d67b37df11628d01d109663082c6da7d5
stinetime <astineman@gamebeast.gg>
2025-06-18 19:00:24 -0500

    Re-ran reading/writing demos for new Data Science (ID: gamebeast-data-science) GCP project, improved documentation

--- src/utils.py
+++ src/utils.py
@@ -1,9 +1,29 @@
-# Use utils.py to define useful functions which will be used in main.py
+# GCP read/write functions
 
-import itertools
+from google.cloud import storage
+from io import StringIO
 
-def all_column_combos(list):
-    result = []
-    for i in range(1, len(list) + 1):
-        result.extend(itertools.combinations(list, i))
-    return result
+import pandas as pd
+
+def gcp_read_csv(gcp_client_obj, bucket_name, csv_file_name):
+    file = gcp_client_obj.bucket(bucket_name).blob(csv_file_name)
+    data = file.download_as_text()
+    df = pd.read_csv(StringIO(data))
+
+    return df
+
+def gcp_write_csv(gcp_client_obj, dataframe, bucket_name, csv_file_name):
+    file = gcp_client_obj.bucket(bucket_name).blob(csv_file_name)
+
+    test = ''
+    if file.exists():
+        while test not in ['y', 'n']:
+            test = input("You are attempting an overwrite. Proceed? Y/n").lower()
+
+    if test == 'n':
+            print('Write canceled.')
+    else: 
+        csv_buffer = StringIO()
+        dataframe.to_csv(csv_buffer, index = False)
+        file.upload_from_string(csv_buffer.getvalue(), content_type = 'text/csv')
+        print('Write complete.')
